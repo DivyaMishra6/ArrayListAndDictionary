6046689c2ac06b6bc8357672171e1d3d15d9282b
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 19:35:07 +0530

    [Divya] : Welcome to Array List and Dictionary

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ArrayListAndDictionary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ArrayList list = new ArrayList();
+            //list.List();
+
+            Dictionary dictionary = new Dictionary();
+            dictionary.Dict();
+
+        }
+
+
+    }
+}
